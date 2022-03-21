acc91855c92d49b7824073afea93d751ac036fcb
Anthony Twesigye <twesigye10@gmail.com>
2022-03-21 12:12:04 +0300

    modified education indicator

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -11,13 +11,13 @@ create_composite_indicators_cpa_child <- function(input_df) {
                            TRUE ~ "west_nile"),
       i.location_type = case_when(district_name %in% c("kampala") ~ "urban",
                                   TRUE ~ "rural"),
-      i.education_level = case_when(hh_member_education %in% c("no_formal_education") ~ "none",
-                                    hh_member_education %in% c("completed_primary", "incomplete_primary", "incomplete_secondary") ~ "low",
-                                    hh_member_education %in% c("completed_secondary", "incomplete_university", "incomplete_prof_degree", 
+      i.education_level = case_when(hoh_education %in% c("no_formal_education") ~ "none",
+                                    hoh_education %in% c("completed_primary", "incomplete_primary", "incomplete_secondary") ~ "low",
+                                    hoh_education %in% c("completed_secondary", "incomplete_university", "incomplete_prof_degree", 
                                                                "incomplete_voc_training", "completed_voc_training") ~ "middle",
-                                    hh_member_education %in% c("completed_university", "completed_prof_degree") ~ "higher",
-                                    hh_member_education %in% c("other") ~ "other",
-                                    TRUE ~ hh_member_education
+                                    hoh_education %in% c("completed_university", "completed_prof_degree") ~ "higher",
+                                    hoh_education %in% c("other") ~ "other",
+                                    TRUE ~ hoh_education
                                     )
     )
 }
