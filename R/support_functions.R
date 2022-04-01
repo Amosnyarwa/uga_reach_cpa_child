c29c83a9ae5db9e19d282c6df1fcb1f007288925
Anthony Twesigye <twesigye10@gmail.com>
2022-04-01 16:05:13 +0300

    duplicate uuid

--- R/support_functions.R
+++ R/support_functions.R
@@ -215,6 +215,33 @@ extract_other_data_repeats <- function(input_repeat_data, input_survey, input_ch
            so_sm_choices)
 }
 
+# duplicate uuid ----------------------------------------------------------
+
+# check uuids of surveys for duplicates
+check_duplicates_by_uuid <- function(input_tool_data) {
+  input_tool_data %>% 
+    group_by(i.check.uuid) %>% 
+    mutate(rank = row_number()) %>% 
+    filter(rank > 1) %>%  
+    mutate(
+      i.check.type = "remove_survey",
+      i.check.name = "point_number",
+      i.check.current_value = "",
+      i.check.value = "",
+      i.check.issue_id = "duplicate_uuid",
+      i.check.issue = "The uuid: {i.check.uuid} is duplicate in the data",
+      i.check.other_text = "",
+      i.check.checked_by = "",
+      i.check.checked_date = as_date(today()),
+      i.check.comment = "", 
+      i.check.reviewed = "",
+      i.check.adjust_log = "",
+      i.check.uuid_cl = "",
+      i.check.so_sm_choices = "")%>% 
+    dplyr::select(starts_with("i.check"))%>% 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
+
 # survey time check -------------------------------------------------------
 
 # check survey time against expected minimum time and maximum time of the survey
