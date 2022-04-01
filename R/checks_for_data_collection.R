52a6a5df66608563a80d8dce1363dd81db9ec7a6
Anthony Twesigye <twesigye10@gmail.com>
2022-04-01 16:00:34 +0300

    other_specify for outliers

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -17,8 +17,29 @@ df_tool_data <- readxl::read_excel("inputs/UGA2109_Cross_Sectoral_Child_Protecti
          end = as_datetime(end)) %>% 
   filter(assent_child == "yes", respondent_age > 11, respondent_age < 18, i.check.start_date > as_date("2022-01-30"), 
          !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE))
-  )
+  ) %>% 
+  select(-c(starts_with("...15")), -c(starts_with("places_where_child_feels_most_at_risk"),
+                                      starts_with("how_protection_risks_influence_behaviour"),
+                                      starts_with("child_role_with_chronic_or_disability"),
+                                      "end_note",	"consent",	"hoh",	"introduction_to_caregiver",
+                                      "location",	"sex",	"age",	"introduction_to_child",	"arrival",
+                                      "edu_completed",	"hh_size",	"numfamily",	"children_for_mdd",
+                                      "num_children_for_mdd",	"children_school_aged",	"num_children_school_aged",
+                                      "demo_check",	"repeat_intro_one"))
 
+# repeats
+harm_mentioned = readxl::read_excel(path = "inputs/UGA2109_Cross_Sectoral_Child_Protection_Assessment_Child_Data.xlsx", sheet = "harm_mentioned") 
+
+child_age_info = readxl::read_excel(path = "inputs/UGA2109_Cross_Sectoral_Child_Protection_Assessment_Child_Data.xlsx", sheet = "child_age_info")
+
+# join repeats to the main dataset
+df_tool_data_harm_mentioned <- df_tool_data %>% 
+  right_join(harm_mentioned, by = c("_uuid" = "_submission__uuid") ) 
+
+df_tool_data_child_age_info <- df_tool_data %>% 
+  right_join(child_age_info, by = c("_uuid" = "_submission__uuid") ) 
+
+# tool
 df_survey <- readxl::read_excel("inputs/Child_Protection_Assessment_Child_Tool.xlsx", sheet = "survey")
 df_choices <- readxl::read_excel("inputs/Child_Protection_Assessment_Child_Tool.xlsx", sheet = "choices")
 
@@ -90,8 +111,15 @@ df_others_data <- extract_other_data(input_tool_data = df_tool_data,
                                      input_survey = df_survey, 
                                      input_choices = df_choices)
 
+df_others_data_repeats <- extract_other_data_repeats(input_repeat_data = df_tool_data_harm_mentioned, 
+                                                     input_survey = df_survey, 
+                                                     input_choices = df_choices, 
+                                                     input_sheet_name = "harm_mentioned",
+                                                     input_repeat_cols = c("how_protection_risks_influence_behaviour", 
+                                                                           "places_where_child_feels_most_at_risk"))
+
 # combine logic and others checks
-df_combined_checks <- bind_rows(df_logic_checks, df_others_data)
+df_combined_checks <- bind_rows(df_logic_checks, df_others_data, df_others_data_repeats)
 
 # output the resulting data frame
 write_csv(x = df_combined_checks, file = paste0("outputs/", butteR::date_file_prefix(), "_combined_checks_child.csv"), na = "")
