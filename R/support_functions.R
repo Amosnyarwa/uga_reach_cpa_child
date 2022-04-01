b1f730992f7b49d7787ac94bf754358e5a378a88
Anthony Twesigye <twesigye10@gmail.com>
2022-04-01 15:36:09 +0300

    outlier functions

--- R/support_functions.R
+++ R/support_functions.R
@@ -271,6 +271,53 @@ check_time_interval_btn_surveys <- function(input_tool_data, input_min_time) {
     rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
 }
 
+# Outliers ----------------------------------------------------------------
+check_outliers <- function(input_tool_data, input_column, input_lower_limit, input_upper_limit) {
+  input_tool_data %>% 
+    filter(!!sym(input_column) < input_lower_limit | !!sym(input_column) > input_upper_limit) %>% 
+    mutate(i.check.type = "change_response",
+           i.check.name = input_column,
+           i.check.current_value = as.character(!!sym({{input_column}})),
+           i.check.value = "",
+           i.check.issue_id = "logic_c_outlier",
+           i.check.issue = paste(input_column,": ",!!sym({{input_column}}), "seems to be an outlier, needs engagement with enumerator"),
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "",
+           i.check.reviewed = "",
+           i.check.adjust_log = "",
+           i.check.uuid_cl = "",
+           i.check.so_sm_choices = "") %>%
+    ungroup() %>%
+    dplyr::select(starts_with("i.check"))%>%
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
+
+check_outliers_repeats <- function(input_tool_data, input_column, input_lower_limit, input_upper_limit, input_sheet_name) {
+  input_tool_data %>% 
+    filter(!!sym(input_column) < input_lower_limit | !!sym(input_column) > input_upper_limit) %>% 
+    mutate(i.check.sheet = input_sheet_name,
+           i.check.type = "change_response",
+           i.check.name = input_column,
+           i.check.current_value = as.character(!!sym({{input_column}})),
+           i.check.value = "",
+           i.check.index = `_index.y`,
+           i.check.issue_id = "logic_c_outlier",
+           i.check.issue = paste(input_column,": ",!!sym({{input_column}}), "seems to be an outlier, needs engagement with enumerator"),
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "",
+           i.check.reviewed = "",
+           i.check.adjust_log = "",
+           i.check.uuid_cl = "",
+           i.check.so_sm_choices = "") %>%
+    ungroup() %>%
+    dplyr::select(starts_with("i.check"))%>%
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+}
+
 # spatial checks ----------------------------------------------------------
 
 # check for duplicate point numbers
