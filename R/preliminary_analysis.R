5f6d887bb5b14619f8bc827d3feda9e38d851049
Anthony Twesigye <twesigye10@gmail.com>
2022-04-06 11:00:54 +0300

    child_age_info: prepare data and create survey

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -67,8 +67,8 @@ df_main_analysis <- analysis_support_after_survey_creation(input_ref_svy = ,
 # harm_mentioned: prepare data and create survey ------------------------------------------------
 
 df_with_composites_harm_mentioned <- create_composite_indicators_cpa_child(input_df = df_harm_mentioned)
-# split data into host and refugee
 
+# split data into host and refugee
 df_ref_harm_mentioned <- df_with_composites_harm_mentioned %>% 
   filter(status == "refugee")
 
@@ -92,8 +92,32 @@ df_harm_mentioned_analysis <- analysis_support_after_survey_creation(input_ref_s
                                                input_dap = dap %>% filter(variable %in% c("how_protection_risks_influence_behaviour",
                                                                                           "places_where_child_feels_most_at_risk")))
 
-df_child_age_info_analysis <- analysis_support_after_survey_creation(input_ref_svy = df_child_age_info,
-                                               input_host_svy = df_ref_pop,
+# child_age_info: prepare data and create survey ------------------------------------------------
+
+df_with_composites_child_age_info <- create_composite_indicators_cpa_child(input_df = df_child_age_info)
+
+# split data into host and refugee
+df_ref_child_age_info <- df_with_composites_child_age_info %>% 
+  filter(status == "refugee")
+
+df_host_child_age_info <- df_with_composites_child_age_info %>% 
+  filter(status == "host_community")
+
+# attach weights
+df_ref_with_weights_child_age_info <- df_ref_child_age_info %>% 
+  left_join(df_ref_with_weights %>% select(uuid, strata, weights), by = "uuid")
+
+df_host_with_weights_child_age_info <- df_host_child_age_info %>% 
+  left_join(df_host_with_weights %>% select(uuid, strata, weights), by = "uuid")
+
+# set up design objects
+
+ref_svy_child_age_info <- as_survey(.data = df_ref_with_weights_child_age_info, strata = strata, weights = weights )
+host_svy_child_age_info <- as_survey(.data = df_host_with_weights_child_age_info, strata = strata, weights = weights )
+
+
+df_child_age_info_analysis <- analysis_support_after_survey_creation(input_ref_svy = ref_svy_child_age_info,
+                                               input_host_svy = host_svy_child_age_info,
                                                input_dap = dap %>% filter(variable %in% c("ages_for_minor")))
 
 # merge analysis ----------------------------------------------------------
