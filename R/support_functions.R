049bc78026dcf2a60912c7d97d76f778b9b758e6
Anthony Twesigye <twesigye10@gmail.com>
2022-04-06 10:45:45 +0300

    analysis_support_after_survey_creation

--- R/support_functions.R
+++ R/support_functions.R
@@ -723,3 +723,90 @@ analysis_support <- function(input_df_cleaned, input_df_ref_pop, input_df_host_p
   
   bind_rows(outputs)
 }
+analysis_support_after_survey_creation <- function(input_ref_svy, input_host_svy, input_dap) {
+  
+  # store analyses
+  outputs <-list()
+  
+  # refugee -----------------------------------------------------------------
+  
+  dap_refugee <- input_dap %>% 
+    filter(split %in% c("all", "refugee_only"))
+  
+  # no subsets
+  refugee_variables_no_subsets <- dap_refugee %>% 
+    pull(variable) %>% unique()
+  
+  # refugee overall, no additional subset
+  outputs$ref_overall <- butteR::survey_collapse(df = input_ref_svy,
+                                                 vars_to_analyze = refugee_variables_no_subsets) %>% 
+    mutate(population = "refugee")
+  
+  #  subsets
+  dap_refugee_subset1 <- input_dap %>%
+    filter( split %in%  c("all","refugee_only"), !is.na(subset_1))
+  
+  # refugee overall, subset 1
+  dap_refugee_subset_split <- dap_refugee_subset1 %>%
+    split(.$subset_1)
+  
+  ref_overall_subset1 <-list()
+  
+  for(i in seq_along(dap_refugee_subset_split)){
+    print(i)
+    subset_temp <- dap_refugee_subset_split[[i]]
+    subset_value <- unique(subset_temp$subset_1)
+    vars_temp <- subset_temp %>% pull(variable)
+    ref_overall_subset1[[subset_value]] <- butteR::survey_collapse(df = input_ref_svy,
+                                                                   vars_to_analyze = vars_temp ,
+                                                                   disag = c( subset_value)
+    )
+  }
+  
+  outputs$ref_overall_subset1 <- bind_rows(ref_overall_subset1) %>%
+    mutate(population = "refugee")
+  
+  
+  # host -----------------------------------------------------------------
+  
+  dap_host <- input_dap %>%
+    filter(split %in%  c("all", "host_only"))
+  
+  # no subsets
+  host_variables_no_subsets <- dap_host %>%
+    pull(variable) %>% unique()
+  
+  # host overall, no additional subset
+  outputs$host_overall <- butteR::survey_collapse(df = input_host_svy,
+                                                  vars_to_analyze = host_variables_no_subsets ) %>%
+    mutate(population = "host")
+  
+  # subsets
+  dap_host_subset1 <- input_dap %>%
+    filter( split %in%  c("all", "host_only"), !is.na(subset_1))
+  
+  dap_host_subset_split <- dap_host_subset1 %>%
+    split(.$subset_1)
+  
+  # host overall, subset 1
+  
+  host_overall_subset1 <- list()
+  
+  for(i in seq_along(dap_host_subset_split)){
+    print(i)
+    subset_temp <- dap_host_subset_split[[i]]
+    subset_value <- unique(subset_temp$subset_1)
+    vars_temp <- subset_temp %>% pull(variable)
+    host_overall_subset1[[subset_value]] <- butteR::survey_collapse(df = input_host_svy,
+                                                                    vars_to_analyze = vars_temp ,
+                                                                    disag = c(subset_value)
+    )
+  }
+  
+  outputs$host_subset1 <- bind_rows(host_overall_subset1) %>%
+    mutate(population = "host")
+  
+  # merge analysis ----------------------------------------------------------
+  
+  bind_rows(outputs)
+}
